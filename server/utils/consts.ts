608b1a000d4bcdc354d344ae517c81ee420da732
JulSeb42 <julien.sebag@me.com>
2022-11-10 12:05:24 +0100

    Clean

--- server/utils/consts.ts
+++ server/utils/consts.ts
@@ -3,6 +3,6 @@
 import "dotenv/config"
 
 export const MONGODB_URI =
-    process.env.MONGODB_URI || "mongodb://localhost/base-apollo-jwt"
+    process.env.MONGODB_URI || "mongodb://localhost/empty-apollo-tsx"
 
 export const PORT = process.env.PORT || 5005
